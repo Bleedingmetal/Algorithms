52553b4b10992b9f2f10395fe0a2df1dbd7d2002
BleedingMetal <adityamanojkrishna2005@gmail.com>
2024-10-22 15:18:43 -0400

    Checking for input validity or returning error till input is valid

--- Homework1.java
+++ Homework1.java
@@ -1,5 +1,18 @@
 import java.util.*;
+
 public class Homework1 {
-    Scanner sc = new Scanner(System.in);
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Do you want to go first or second? 1/2? \n");
+        int order = sc.nextInt();
+        for (int i = 1; i > 0; i++) {
+            if (order <= 2) {
+                break;
+            } else {
+                System.out.println("Option is out of bounds please choose either 1 or 2\n");
+                order = sc.nextInt();
+                continue;
+            }
+        }
+    }
 }
